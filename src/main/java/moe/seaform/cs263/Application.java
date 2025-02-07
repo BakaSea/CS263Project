829da2f23b79cb0a6c9ed0552206a7a6d86b703e
BakaSea <sea@infstudio.net>
2025-02-07 15:20:19 -0800

    Setup

---
+++ src/main/java/moe/seaform/cs263/Application.java
@@ -0,0 +1,22 @@
+package moe.seaform.cs263;
+
+import pascal.taie.Main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Application {
+
+    public static void main(String args[]) {
+        if (args.length > 0) {
+            List<String> argList = new ArrayList<>();
+            Collections.addAll(argList, "-pp", "-p", "plan.yml");
+            Collections.addAll(argList, args);
+            Main.main(argList.toArray(new String[0]));
+        } else {
+            System.out.println("Commands error!");
+        }
+    }
+
+}
